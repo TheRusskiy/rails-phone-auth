dbb641c8d543d197a4e596f373c02bcf748ee451
therusskiy <dimanrussian@gmail.com>
2021-05-06 01:25:50 +0800

    PhoneVerificationController

---
+++ app/controllers/phone_verification_controller.rb
@@ -0,0 +1,51 @@
+class PhoneVerificationController < ApplicationController
+  def send_verification
+    PhoneVerification.send_verification(phone: params[:phone])
+    render json: {
+      error: nil
+    }
+  rescue => e
+    render json: { error: e.message }
+  end
+
+  def code_to_phone_token
+    phone_token = PhoneVerification.code_to_phone_token(
+      phone: params[:phone],
+      code: params[:code]
+    )
+    render json: {
+      phoneToken: phone_token,
+      error: nil
+    }
+  rescue => e
+    render json: { error: e.message }
+  end
+
+  def login
+    phone = PhoneVerification.phone_token_to_phone(params[:phoneToken])
+
+    user = User.find_by_phone(phone)
+
+    render json: {
+      user: user,
+      error: nil
+    }
+  rescue => e
+    render json: { error: e.message }
+  end
+
+  def signup
+    phone = PhoneVerification.phone_token_to_phone(params[:phoneToken])
+
+    user = User.find_or_create_by(phone: phone) do |u|
+      u.name = params[:name]
+    end
+
+    render json: {
+      user: user,
+      error: nil
+    }
+  rescue => e
+    render json: { error: e.message }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post 'send-phone-verification', to: 'phone_verification#send_verification'
+  post 'verify-phone', to: 'phone_verification#code_to_phone_token'
+  post 'login', to: 'phone_verification#login'
+  post 'signup', to: 'phone_verification#signup'
 end
